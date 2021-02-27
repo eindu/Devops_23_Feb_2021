58fca1a3db9d7bf58556fbdf7d7116e3f0349fd9
eindu <indue69@gmail.com>
2021-02-27 19:09:25 +0530

    Demo file added

---
+++ FirstApp/src/net/codejava/FirstApp.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+public class FirstApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Programm");
+
+	}
+
+}
